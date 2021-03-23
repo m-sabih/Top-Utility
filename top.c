b0816b56a08c6545dc248bb57fb1ec4d51dd4f5e
sabih544 <muhammadsabih56@gmail.com>
2021-03-23 17:29:52 +0500

    first line completion

--- top.c
+++ top.c
@@ -6,39 +6,38 @@
 #include <time.h>
 #include <linux/kernel.h>
 #include <sys/sysinfo.h>
+#include <pwd.h>
+
+#include<sys/utsname.h>
 #include<utmp.h>
 
-extern int errno;
 
-char* getCurrentTime();
-char* getTimeSinceBoot();
-char* getUserCount();
-char* getLoadAverage();
-char* loadavg();
+extern int errno;
+void read_dir(char *);
+void getCurrentTime();
+void getTimeSinceBoot();
+void getUserCount();
+void getLoadAverage();
+void loadavg();
 
 int main(int argc, char *argv[]){
-	//read_dir("/proc/");
 	printf("top - ");
-	char* currentTime = getCurrentTime();
+	getCurrentTime();
 	getTimeSinceBoot();
 	getUserCount();
-	char* load = loadavg();
-	printf("%s", load);
+	loadavg();
 
 	printf("\n");
 	return 0;
 }
 
-char* getCurrentTime(){
-	char *curTime = malloc(10);
+void getCurrentTime(){
 	time_t currentTime = time(0);
   	struct tm localTime = *localtime(&currentTime);
-   	sprintf(curTime,"%02d:%02d:%02d",localTime.tm_hour, localTime.tm_min, localTime.tm_sec);
-	return curTime;
+   	printf("%02d:%02d:%02d",localTime.tm_hour, localTime.tm_min, localTime.tm_sec);
 }
 
-char* getTimeSinceBoot(){
-	char *upTime = malloc(5);
+void getTimeSinceBoot(){
 	int hour, min, upTimeInSec;
 	struct sysinfo s_info;
     int error = sysinfo(&s_info);
@@ -49,34 +48,29 @@ char* getTimeSinceBoot(){
     upTimeInSec = s_info.uptime;
     hour = (upTimeInSec / 3600);
   	min = (upTimeInSec - (3600 * hour)) / 60;
-    printf(" up %d:%d",hour,min);
-    sprintf(upTime,"%d:%d",hour,min);
-	return upTime;
+    printf(" up %d:%d,  ",hour,min);
 }
 
-char* getUserCount() {
-	char *upTime = malloc(5);
-	struct utmp * loggedUsers;
-	setutent();
-	loggedUsers = getutent();
-	int userCount = 0;
-	while (loggedUsers) {
+void getUserCount() {
+  	struct utmp * loggedUsers;
+  	setutent();
+  	loggedUsers = getutent();
+  	int userCount = 0;
+  	while (loggedUsers) {
 	    if (loggedUsers -> ut_type == USER_PROCESS) {
-      	userCount++;
-    }
+    	  userCount++;
+    	}
     loggedUsers = getutent();
-  }
-  	printf(",  %d user", userCount);
+  	}
+  	printf("%d user,  ", userCount);
 }
 
-char* getLoadAvg(void){
+void loadavg(void){
 	double avgs[3];
-	char *str = malloc(15);
 
 	if (getloadavg(avgs, 3) < 0) {
 		perror("getloadavg");
 		exit(1);
 	}
-	sprintf(str,"%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
-	return str;
+	printf("load average: %.2f, %.2f, %.2f \n", avgs[0], avgs[1], avgs[2]);	
 }
